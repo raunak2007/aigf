b1b8c298c65a2d0dadca0cb1f65de0ba10997d8c
gwang1224 <gracewang187@gmail.com>
2023-09-10 20:10:35 -0700

    bootstrap try 2

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,12 @@ export default function Home() {
         <title>AIsha</title>
       </Head>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
       <ul class="navbar">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
