74941d701c46eed6e8850936ba152b357a4381fd
gwang1224 <gracewang187@gmail.com>
2023-09-10 23:19:21 -0700

    text

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h2>Dating in the 21st century is hard. That's why we've created an AI girlfriend...</h2>
-        <h2>a revolutionary digital companion designed to provide companionship, support, and genuine connection in an increasingly complex world of relationships.</h2>
+        <h2>a revolutionary digital companion designed to provide companionship, support, and genuine connection in a complex world of relationships.</h2>
         <h3>Talk to AIsha</h3>
         <form onSubmit={onSubmit}>
           <input
